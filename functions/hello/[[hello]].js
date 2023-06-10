ed6fde6e1168467bb151ebb974c3b72eeb807407
Richard Hildred <rhildred@gmail.com>
2023-06-09 22:41:38 -0400

    put fetch in the worker2functionAdapter

--- functions/hello/[[hello]].js
+++ functions/hello/[[hello]].js
@@ -2,5 +2,5 @@ import worker from '../../workers/hello.js';
 import worker2functionAdapter from '../../src/worker2functionAdapter.js';
 
 export async function onRequest(context){
-    return worker2functionAdapter(worker.fetch, context);
+    return worker2functionAdapter(worker, context);
 }
