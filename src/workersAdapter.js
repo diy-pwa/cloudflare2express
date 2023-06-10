c85937a7ecbc355a0bc0a7cbdce9b4e9a6931cc9
Richard Hildred <rhildred@gmail.com>
2023-06-09 22:39:35 -0400

    put fetch in workersAdapter

--- src/workersAdapter.js
+++ src/workersAdapter.js
@@ -1,5 +1,5 @@
 import prepareRequest from "./prepareRequest.js";
 import sendResponse from "./sendResponse.js";
 export default async function(delegate, req, res){
-    sendResponse(await delegate(prepareRequest(req)), res);
+    sendResponse(await delegate.fetch(prepareRequest(req)), res);
 }
