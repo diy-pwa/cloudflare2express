fdb5651c22fbed53025425138962525d74440e54
Rich Hildred <rhildred@wlu.ca>
2023-07-04 16:41:58 -0400

    get functions from folder

---
+++ __tests__/FunctionsApp.test.js
@@ -0,0 +1,20 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+import request from 'supertest';
+import createApp from '../src/FunctionsApp.js';
+import fs from 'fs';
+
+beforeEach(async()=>{
+    await fs.promises.rm("test2", { recursive: true, force: true });
+});
+
+describe("tests for sample express functions app", ()=>{
+    it("handles a redirect", async ()=>{
+        const modules = import.meta.glob('../functions/*/*.js', { import: 'onRequest',
+        eager: true,
+    });
+        const app = createApp('../functions/*/*.js', modules);
+        const res = await request(app).get("/corsproxy/github.com");
+        expect(res.status).toBe(200);
+        expect(res.headers['access-control-allow-origin']).toBe("*");
+    });
+})
--- functions/corsproxy/[[corsproxy]].js
+++ functions/corsproxy/[[corsproxy]].js
@@ -1,5 +1,6 @@
 import {CorsProxyResponse} from "@rhildred/cors-proxy2";
 
 export async function onRequest(context) {
+    context.env.url = context.request.url.replace(/^.*corsproxy/, "https:/");
     return await CorsProxyResponse.fetch(context.request, context.env);
 }
